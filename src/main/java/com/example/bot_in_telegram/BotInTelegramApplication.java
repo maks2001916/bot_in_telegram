48213be6075445a915e34a06bdcde1d14c43a71a
maks <mamonov.maksim.20003@gmail.com>
2023-03-09 19:03:20 +0500

    Initial commit

---
+++ src/main/java/com/example/bot_in_telegram/BotInTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.example.bot_in_telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotInTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotInTelegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bot_in_telegram/BotInTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bot_in_telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotInTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
